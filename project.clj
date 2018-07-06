a664f4483c0ae2f954888ff53f7ae88b14830f5e
Burin Choomnuan <agilecreativity@gmail.com>
2018-07-05 23:56:49 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject data-fetcher "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljfmt "0.5.7"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [commons-io/commons-io "2.6"]
+                 [akvo/fs "20180618-165206.5f3fb50b"] ])
---
+++ src/data_fetcher/core.clj
@@ -0,0 +1,33 @@
+(ns data-fetcher.core
+  (:require
+   [me.raynes.fs.compression :as fsc]
+   [me.raynes.fs :as fs])
+  (:import
+   [org.apache.commons.io FileUtils]
+   [java.net URL]))
+
+(def mnist-sample "https://s3.us-east-2.amazonaws.com/mxnet-scala/scala-example-ci/mnist/mnist.zip")
+
+(defn download-file
+  [remote-url local-path & {:keys [override?]
+                            :or {override? false}}]
+  (let [remote-file (URL. remote-url)
+        local-file (fs/file (fs/expand-home local-path))]
+    (if-not (or (fs/exists? remote-url) override?)
+      (FileUtils/copyURLToFile remote-file local-file)
+      (println "File already exist locally, will be skipped."))))
+
+;; Note: the directory will be created automatically!
+#_ (download-file mnist-sample
+                  "/Users/bchoomnu/Downloads/zzzz/mnist-demo.zip")
+
+
+;; TODO: handle different type of input e.g.
+;; .tar.gz as well as .zip or .rar, etc!
+(defn extract-file
+  [input-file output-dir]
+  (fsc/unzip (fs/expand-home input-file)
+             (fs/expand-home output-dir)))
+
+#_ (extract-file "~/Downloads/mnist-demo.zip"
+                 "~/Downloads/TTTT")
