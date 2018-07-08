d3ec9bc1790596a250d32df308883f951a052767
Burin Choomnuan <agilecreativity@gmail.com>
2018-07-07 22:59:30 -0400

    WIP

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject data-fetcher "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject data-fetcher "0.1.0"
+  :description "Simple library to fetch remote data easily."
+  :url "https://github.com/agilecreativity/data-fetcher"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljfmt "0.5.7"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [commons-io/commons-io "2.6"]
                  [clj-http "3.9.0"]
-                 [akvo/fs "20180618-165206.5f3fb50b"] ])
+                 [akvo/fs "20180618-165206.5f3fb50b"]])
